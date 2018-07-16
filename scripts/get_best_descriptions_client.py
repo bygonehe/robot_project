45968842e71fea5c47ee780c35bfd4ba0302a6fc
bygonehe <34040723+bygonehe@users.noreply.github.com>
2018-07-16 10:17:18 -0700

    Add files via upload

--- scripts/get_best_descriptions_client.py
+++ scripts/get_best_descriptions_client.py
@@ -2,6 +2,9 @@
 
 import sys
 import rospy
+import random
+import numpy as np
+from beginner_tutorials.msg import *
 from beginner_tutorials.srv import *
 
 def get_best_descriptions_client(x, y, z):
@@ -9,7 +12,7 @@ def get_best_descriptions_client(x, y, z):
     try:
         get_best_descriptions = rospy.ServiceProxy('get_best_descriptions', GetDescriptionsOutput)
         resp1 = get_best_descriptions(x, y, z)
-        return resp1
+        return resp1.Outputfeature
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
 
@@ -17,12 +20,47 @@ def usage():
     return "%s [x y]"%sys.argv[0]
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4:
-        x = float(sys.argv[1])
-        y = int(sys.argv[2])
-        z = float(sys.argv[3])
-    else:
-        print usage()
-        sys.exit(1)
+
+
+    z1 = Object()
+    z1.name = 'Banana'
+    z1.color = [0.2, 0.3, 0.5]
+    z1.shape = 0.21
+    z1.size = 0.6
+    z1.position = [0.8, 0, 0]
+    z1_feature_list = [z1.color, z1.shape, z1.size, z1.position]
+
+
+    z2 = Object()
+    z2.name = 'Apple'
+    z2.color = [0.7, 0.9, 0.5]
+    z2.shape = 0.87
+    z2.size = 0.1
+    z2.position = [0.6, 0.5, 0.1]
+    z2_feature_list = [z2.color, z2.shape, z2.size, z2.position]
+
+    z = [z1,z2]
+    print(len(z))
+    z_feature_list = [0]*len(z)
+    ztotal=[0]*len(z)
+    for i in range(0,len(z)):
+        ztotal[i]=[z[i].color,z[i].shape,z[i].size,z[i].position]
+    print(z)
+    #Output = OutputObject()
+    x = [0,1,2,3]
+    print(type(x))
+    print(z[0])
+
+    #for i in range(0,len(z)):
+    #    m[i]=[z[i].color,z[i].shape,z[i].size,z[i].position]
+    #print(m)
+
+
+    Output=get_best_descriptions_client(z,x, z[0])
+    print(Output)
+    #output=list(Output)
+
+
+
     print "Requesting get best descriptions"
-    print "Best decsrption = %s"%(get_best_descriptions_client(x, y, z))
+    print (Output)
--- scripts/get_best_descriptions_server.py
+++ scripts/get_best_descriptions_server.py
@@ -1,40 +1,47 @@
 #!/usr/bin/env python
 
+from beginner_tutorials.msg import *
 from beginner_tutorials.srv import *
+import numpy as np
 import rospy
 
+
 #Get the probability function, firstly we need to get the error probability and then the probability
-def get_probability(feature_index, object_list, target_object):
+def get_probability(feature_index, testobject, target_object):
     if feature_index == 1 or feature_index == 2: # size and shape only has one element
-        x = len(object_list) # Get the number of object
-        bar = object_list[0:len(object_list),feature_index] # Extract the feature value of corresponding index for all objects
-        dif=abs(bar-target_object[0][feature_index]) #target_object[0][feature_index] means the feature value of corr. index for target object
+        x = len(testobject) #Get the number of objects
+        bar=[0]*x
+        dif=[0]*x
+        for i in range (0,x):
+            bar[i]=testobject[i][feature_index] #Get the corresponding objects' feature value
+            dif[i]=abs(bar[i]-target_object[feature_index]) #Get the difference value
+        dif = np.array(dif)
         if sum(dif)==0:
             return('The probability is 100% and is uniqle')
-        norm_error_prob = dif/(sum(dif)) # Get the normal error prob.
-        corr_prob = (1-norm_error_prob) # Get the corr. prob
-        norm_prob = corr_prob/(sum(corr_prob)) # Get the normal corr. prob
+        norm_error_prob = dif/(sum(dif))
+        corr_prob = (1-norm_error_prob)
     if feature_index == 0 or feature_index == 3: # color have 3 elements: R,G,B, norm1 # position have 3 elements: X,Y and Z, in the case of calculating distance error, norm2
-        x = len(object_list) # number of objects
-        bar = object_list[0:len(object_list),feature_index] # Extract the feature value of corresponding index for all objects, here we have three elements value for each object
-        dif = [[0]*1 for i in range(x)]
-        if feature_index == 0: # color probability: using norm1
+        x = len(testobject)
+        bar=[0]*x
+        dif=[0]*x
+        for i in range (0,x):
+            bar[i]=testobject[i][feature_index]
+        if feature_index == 0: # color probability: norm1
             for i in range(0,x):
-                temp_dif = (abs((bar[i])-target_object[0][feature_index]))
+                temp_dif=abs(bar[i]-np.array(target_object[feature_index]))
                 dif[i] = sum(temp_dif)
             if sum(dif)==0:
                 return('The probability is 100% and is uniqle')
             norm_error_prob = dif/sum(dif)  
-        if feature_index == 3: # position probability: using norm2
+        if feature_index == 3: # position probability: norm2
             for i in range(0,x):
-                temp_dif = (abs((bar[i])-target_object[0][feature_index])) 
+                temp_dif=abs(bar[i]-np.array(target_object[feature_index])) 
                 dif[i] = np.sqrt(temp_dif[0]**2+temp_dif[1]**2+temp_dif[2]**2)
             if sum(dif)==0:
                 return('The probability is 100% and is uniqle')
             norm_error_prob = dif/sum(dif)
-        corr_prob = (1-norm_error_prob)
-        norm_prob = corr_prob/(sum(corr_prob))           
-    return (norm_prob)
+        corr_prob = (1-norm_error_prob)          
+    return (corr_prob)
 
 # get_possible_descriptions:
 # Given a list of objects and a target object (which is probably contained within the list of objects), you should find
@@ -44,9 +51,22 @@ def get_probability(feature_index, object_list, target_object):
 # are distinctive. Then you should return two possible descriptions. On the other hand, one single feature might not 
 # be sufficient to describe the object. Then you should return (all possible) pairs of features which can distinctively 
 # describe the target object and so forth.
-def handle_get_best_descriptions(object_list, feature_list, target_object): #feature_list = [0,1,2,3]
+def handle_get_best_descriptions(req):
+    Object_list = req.Object_LIST
+    object_list = [0]*len(Object_list)
+    for i in range(0,len(Object_list)):
+        object_list[i]=[Object_list[i].color,Object_list[i].shape,Object_list[i].size,Object_list[i].position]
+
+
+    target_Object = req.Target_object
+    target_object=[target_Object.color,target_Object.shape,target_Object.size,target_Object.position]
+
+    feature_list = req.Feature_list
+
+
+ #feature_list = [0,1,2,3]
     #feature_list = [0,1,2,3] represents color, size, shape, position individually
-    NO_OBJECT = ('NO CORRESPONDING OBJECT')
+    NO_OBJECT = ['NO CORRESPONDING OBJECT']
     number_of_feature = len(feature_list)
     number_of_object_list = len(object_list)
     loop = 1
@@ -75,7 +95,7 @@ def handle_get_best_descriptions(object_list, feature_list, target_object): #fea
         else:
             feature_choice = feature_choice[:-record]
         print('One feature is used to describe')
-        return(feature_choice)
+        return([feature_choice])
 
 # check whether two features can describe
     random_max_number_choice = 10
@@ -89,8 +109,8 @@ def handle_get_best_descriptions(object_list, feature_list, target_object): #fea
             prob1 = get_probability(t1, object_list, target_object)
             prob2 = get_probability(t2, object_list, target_object)
             combination_prob = (prob1*prob2)
-            norm_prob = combination_prob/sum(combination_prob)
-            if max(norm_prob)>=0.6: # 0.6 is a set threshold value
+            #norm_prob = combination_prob/sum(combination_prob)
+            if max(combination_prob)>=0.6: # 0.6 is a set threshold value
                 pairs_possible[q] = [t1,t2] # Recording the satifised pairs
                 q=q+1
             else:
@@ -103,7 +123,7 @@ def handle_get_best_descriptions(object_list, feature_list, target_object): #fea
                 record=record+1
         pairs_possible = pairs_possible[:-record]
         print('TWO features combination are used to describe')
-        return(pairs_possible)
+        return([pairs_possible])
 
 # check whether three features can describe
     combination_prob=0
@@ -120,8 +140,8 @@ def handle_get_best_descriptions(object_list, feature_list, target_object): #fea
                 prob2 = get_probability(t2, object_list, target_object)
                 prob3 = get_probability(t3, object_list, target_object)
                 combination_prob = prob1*prob2*prob3
-                norm_prob = combination_prob/sum(combination_prob)
-                if max(norm_prob)>=0.6:
+                #norm_prob = combination_prob/sum(combination_prob)
+                if max(combination_prob)>=0.6:
                     triple_possible[q] = [t1,t2,t3]
                     q=q+1
                 else:
@@ -134,7 +154,7 @@ def handle_get_best_descriptions(object_list, feature_list, target_object): #fea
                 record=record+1
         triple_possible = triple_possible[:-record]
         print('THREE features combination are used to describe')
-        return(triple_possible)
+        return([triple_possible])
     
     
 # check whether four features can describe   
@@ -155,8 +175,8 @@ def handle_get_best_descriptions(object_list, feature_list, target_object): #fea
                     prob3 = get_probability(t3, object_list, target_object)
                     prob4 = get_probability(t4, object_list, target_object)
                     combination_prob = prob1*prob2*prob3*prob4
-                    norm_prob = combination_prob/sum(combination_prob)
-                    if max(norm_prob)>=0.6:
+                    #norm_prob = combination_prob/sum(combination_prob)
+                    if max(combination_prob)>=0.6:
                         quadra_possible[q] = [t1,t2,t3,t4]
                         q=q+1
                     else:
@@ -169,7 +189,7 @@ def handle_get_best_descriptions(object_list, feature_list, target_object): #fea
                 record=record+1
         quadra_possible = quadra_possible[:-record]
         print('FOUR features combination are used to describe')
-        return(quadra_possible)
+        return([quadra_possible])
 
 
 def get_best_descriptions_server():
